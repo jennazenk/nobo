760b5658ee407b7231ce4d3cee7e6523e1e0e46e
Jenna Zenk <zenkjenna@gmail.com>
2016-08-31 18:55:19 +0200

    quantity and total price

--- browser/js/offer/offer.controller.js
+++ browser/js/offer/offer.controller.js
@@ -1,7 +1,15 @@
 'use strict';
 
-nobo.controller('OfferCtrl', function ($scope, $state) {
-	$scope.quantities = [1,2,3,4,5,6,7,8,9,10];
+nobo.controller('OfferCtrl', function($scope, $state) {
+
+    $scope.quantities = [2, 3, 4, 5, 6, 7, 8, 9];
+
+    $scope.offers = {
+        "Besoin ponctuel": 32,
+        "Pack 10h": 280,
+        "Pack 20h": 480
+    }
+
+    $scope.checkoutMode = false;
+
 });
-
-
